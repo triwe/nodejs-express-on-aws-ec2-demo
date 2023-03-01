801ef6ece64cfc23bd8be4f917ce801532712e57
trissy <105899557+triwe@users.noreply.github.com>
2023-03-01 15:46:34 -0700

    Update app.js

--- app.js
+++ app.js
@@ -20,5 +20,5 @@ app.get('/products', (req, res) => {
 })
 
 app.listen(port, ()=> {
-  console.log(`Demo app is up and listening to port: ${port}`);
+  console.log(`Demo app is live and listening to port: ${port}`);
 })
